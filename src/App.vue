147b5b58d03dec7f2620efbbe74f9df9620fd8cc
Benjamin Krause <benjamin.krause@shinefour.de>
2020-11-15 23:15:50 +0100

    Adjust app base design and home site

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,67 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/contacts">Contacts</router-link> |
-      <router-link to="/about">About</router-link>
+  <main>
+    <div id="app">
+      <router-view />
     </div>
-    <router-view />
-  </div>
+    <nav>
+      <div>
+        <router-link to="/">Home</router-link>
+        <router-link to="/contacts">Contacts</router-link>
+        <router-link to="/about">About</router-link>
+      </div>
+    </nav>
+  </main>
 </template>
 
 <style lang="scss">
+body {
+  padding: 0;
+  margin: 0;
+  margin-bottom: 35px;
+  position: relative;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  max-width: 750px;
+  width: 100%;
+  height: 100%;
+  margin: 0 auto;
 }
 
-#nav {
-  padding: 30px;
+nav {
+  background-color: rgb(220, 54, 100);
+  position: fixed;
+  display: block;
+  bottom: 0;
+  width: 100%;
+  div {
+    position: relative;
+    left: 50%;
+    transform: translate(-50%, 0);
+    height: 100%;
+    width: fit-content;
+    a {
+      display: inline-block;
+      font-weight: bold;
+      color: #2c3e50;
+      border-right: 1px solid black;
+      padding: 0 10px;
+      height: 30px;
+      line-height: 30px;
+      text-align: center;
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+      &.router-link-exact-active {
+        color: white;
+      }
 
-    &.router-link-exact-active {
-      color: #42b983;
+      &:last-child {
+        border: 0;
+      }
     }
   }
 }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,23 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <h1>Outlawraspberry Contact Diary</h1>
+    <h2>Track your social contacts</h2>
+    <img alt="Outlawraspberry Logo" src="../assets/outlawraspberry-logo.png" />
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
 
 @Component({
-  components: {
-    HelloWorld
-  }
+  components: {}
 })
 export default class Home extends Vue {}
 </script>
+
+<style lang="scss" scoped>
+img {
+  width: 80%;
+  max-width: 300px;
+}
+</style>

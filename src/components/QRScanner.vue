02acf07741208ebf5a31f0bee5b7e6a2fdab2f3c
Benjamin Krause <benjamin.krause@outlawraspberry.de>
2020-11-16 11:13:46 +0100

    Add base for qrcode scanner

---
+++ src/components/QRScanner.vue
@@ -0,0 +1,80 @@
+<template>
+  <div id="reader">
+    <p>{{ this.content }}</p>
+    <video id="camera-view"></video>
+    <canvas id="can"></canvas>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+import jsQR from "jsqr";
+
+@Component
+export default class QRScanner extends Vue {
+  private stream: any;
+  private vid: any;
+  private can: any;
+  private canCon: any;
+  private content: any = "";
+  async mounted() {
+    this.can = document.querySelector("#can");
+    this.canCon = this.can.getContext("2d");
+    this.vid = document.querySelector("#camera-view");
+    this.startScan().catch(err => {
+      this.content = "No camera can be found!";
+      console.log(err);
+    });
+  }
+
+  private async startScan() {
+    return new Promise((resolve, reject) => {
+      const func = async () => {
+        this.stream = await navigator.mediaDevices
+          .getUserMedia({
+            video: { facingMode: "environment" }
+          })
+          .catch(err => {
+            reject(err);
+          });
+        this.vid.srcObject = this.stream;
+        this.vid.setAttribute("playsinline", true);
+        this.vid.play();
+        console.log("fghdhfgdsfghdhfg");
+        requestAnimationFrame(this.scan.bind(this));
+      };
+      func();
+    });
+  }
+
+  private async scan() {
+    if (this.vid.readyState === this.vid.HAVE_ENOUGH_DATA) {
+      this.can.height = this.vid.videoHeight;
+      this.can.width = this.vid.videoWidth;
+
+      this.canCon.drawImage(this.vid, 0, 0, this.can.width, this.can.height);
+
+      const data = this.canCon.getImageData(
+        0,
+        0,
+        this.can.width,
+        this.can.height
+      );
+
+      const code = jsQR(data.data, data.width, data.height, {
+        inversionAttempts: "dontInvert"
+      });
+      if (code) {
+        this.content = code;
+      } else {
+        requestAnimationFrame(this.scan);
+      }
+    } else {
+      requestAnimationFrame(this.scan);
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss"></style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,18 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <QRScanner></QRScanner>
   </div>
 </template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import QRScanner from "@/components/QRScanner.vue";
+
+@Component({
+  components: {
+    QRScanner
+  }
+})
+export default class Contacts extends Vue {}
+</script>

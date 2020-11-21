457f5c6cd02193f8afd918062fe587843cbd5f65
Benjamin Krause <benjamin.krause@outlawraspberry.de>
2020-11-21 16:30:31 +0100

    Remove google url from the service worker

---
+++ removeGoogleFromServiceWorker.js
@@ -0,0 +1,22 @@
+const fs = require("fs");
+
+const serviceWorkerPath = "./dist/service-worker.js";
+const files = fs.readdirSync("./dist");
+let manifestPath;
+
+files.forEach(path => {
+  if (path.search("precache-manifest") == 0) {
+    manifestPath = `dist/${path}`;
+  }
+});
+
+const file = fs.readFileSync(serviceWorkerPath, "UTF-8");
+const lines = file.split(/\r?\n/);
+lines[0] = `importScripts("${manifestPath}");`;
+let newContent = "";
+
+lines.forEach(line => {
+  newContent += `${line}\n`;
+});
+
+fs.writeFileSync(serviceWorkerPath, newContent, "UTF-8");
